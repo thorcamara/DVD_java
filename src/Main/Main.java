e987704b759e7763730be443d0d03149c3c82045
NotLuan <you@example.com>
2024-04-03 21:56:27 -0300

    mudancas

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,11 +3,11 @@ package Main;
 public class Main {
     public static void main(String[] args) {
        
-//        public void cadastraAmigos();
-//        
-//        public void cadastraDVDs();
-//        
-//        public void registraEmprestimos();
+       public void cadastraAmigos();
+
+       public void cadastraDVDs();
+
+       public void registraEmprestimos();
         
     }
 }
